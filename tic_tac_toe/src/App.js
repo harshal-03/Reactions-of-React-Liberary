98781748a7cc6cbae9af37d9d10fad72cec369e3
harshal-03 <harshalgalwecode@gmail.com>
2024-10-26 09:55:44 +0530

    Tic Tac Toe Game using Hooks

---
+++ tic_tac_toe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
